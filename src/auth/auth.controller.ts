8af83ffbdd95c7e3565f5825ba751de253606d21
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2022-12-13 08:59:56 -0300

    feat(auth): implementing local strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  login() {
+    //return this.authService.login();
+  }
+}
+
+//? The guards can be registed on controllers or modules
+//? The local guard will be placed on login route
+//? The JWT will be placed on the other routes
